095ff0f65c7046ae74354670215fff50e4653478
Chris Su <chrissu@Chriss-MacBook-Pro.local>
2015-09-03 11:07:00 -0500

    added facebook.js

--- facebook.js
+++ facebook.js
@@ -1,3 +1,16 @@
 /**
  * Created by chrissu on 9/3/15.
  */
+var FB = require('fb');
+
+var FB = require('fb');
+FB.setAccessToken('CAACEdEose0cBAEVBgHMLSKVtWSHvxQtVXpRNyKODi3ZAyZAk9SMX8LcCM3DHlDgwJJMKdwvFY23hy9WYAep7DW0idae65JUmvYDZB1cZC81nrbqAOAjpaS7z0pia1nGBeCMRFRayLSi4dXsiLWKF5TUb5WerdkcHeu6DtrM8p2Qz47BCDmTnVMRTStkZBd5yZBoGBUnSkeZBQZDZD');
+
+var body = 'My first post using facebook-node-sdk';
+FB.api('me/feed', 'post', { message: body}, function (res) {
+    if(!res || res.error) {
+        console.log(!res ? 'error occurred' : res.error);
+        return;
+    }
+    console.log('Post Id: ' + res.id);
+});
--- twitter.js
+++ twitter.js
@@ -1,3 +1,30 @@
 /**
  * Created by chrissu on 9/3/15.
  */
+
+var fs = require('fs');
+var Twit = require('twit');
+
+var TwitterApp = new Twit({
+    consumer_key:         'lF0toTHTD5BQxbvrfoy87scqz'
+    , consumer_secret:      'ERq30rIPQlouR0qpNKrYDudsdpIzplmTxeAhz9C5RivmovZ8bK'
+    , access_token:         '3529755022-pSnr3zgRME4H1jK7hR6s109LCg1Ace8205EnmCO'
+    , access_token_secret:  'xjmXeXsigPoDBYFY6ZGMDPKds0wQibf8iOZ599k1cs0R1'
+})
+
+//Upload media to Twitter
+
+//The image passed should be the raw binary of the image or binary base64 encoded
+var mediaContent= fs.readFileSync('clock.png', { encoding: 'base64' })
+
+TwitterApp.post('media/upload', { media_data: mediaContent }, function (err, data, response) {
+
+    var mediaIdStr = data.media_id_string
+    console.log(mediaIdStr)
+    console.log(data)
+    var params = { media_ids: [mediaIdStr]}
+
+    TwitterApp.post('statuses/update', params, function (err, data, response) {
+        console.log(data)
+    })
+})
