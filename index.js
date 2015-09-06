7892769488a788f2a5d5db1925735451a304484d
thomasameisel <thomas.meisel95@gmail.com>
2015-09-05 20:13:58 -0500

    add ability to create new login

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 #! /usr/bin/env node
 
-var mysql = require('mysql');
+var fs = require('fs');
 var prompt = require("prompt");
 var _ = require('lodash');
 var readline = require('readline');
 
 var argv = require('yargs')
     .usage('Usage: dropbox [options]')
-    .example('dropbox --serverDirectory dnode://test-data/server --clientDirectory file://test-data/client', '(after launching the dropbox-server) listen for beacon signals with the given receiver id and reporting websocket url')
+    .example('dropbox --serverDirectory test-data/server --clientDirectory test-data/client', '(after launching the dropbox-server) listen for beacon signals with the given receiver id and reporting websocket url')
     .demand(['sd','cd'])
     .alias('sd', 'serverDirectory')
     .nargs('sd', 1)
@@ -26,7 +26,7 @@ var argv = require('yargs')
     .epilog('Apache License V2 2015, Jules White')
     .argv;
 
-
+var db = require('./lib/sync/db');
 var sync = require('./lib/sync/sync');
 var dnodeClient = require("./lib/sync/sync-client");
 var Pipeline = require("./lib/sync/pipeline").Pipeline;
@@ -65,7 +65,8 @@ writePipeline.addAction({
     }
 });
 
-function checkForChanges(username){
+function checkForChanges(){
+
     sync.compare(directories.serverDirectory,directories.clientDirectory,sync.filesMatchNameAndSize, function(rslt) {
 
         rslt.srcPath = directories.serverDirectory;
@@ -76,11 +77,11 @@ function checkForChanges(username){
 }
 
 var timer;
-function scheduleChangeCheck(when,repeat,username){
+function scheduleChangeCheck(when,repeat){
     timer = setTimeout(function(){
-        checkForChanges(username);
+        checkForChanges();
 
-        if(repeat){scheduleChangeCheck(when,repeat,username)}
+        if(repeat){scheduleChangeCheck(when,repeat)}
     },when);
 }
 
@@ -128,10 +129,11 @@ function getUserInput(){
         if(operation == 'quit' || operation == 'logout') {
             rl.close();
             clearTimeout(timer);
-            dnodeClient.end();
             if (operation == 'logout') {
                 console.log("Logged out!\n");
-                promptLogin();
+                promptLoginOrCreate();
+            } else {
+                dnodeClient.end();
             }
             return;
         } else if (operation == 'help') {
@@ -149,27 +151,59 @@ function getUserInput(){
     });
 }
 
-function getPasswordFromDb(username) { return function(queryFinished) {
-        var connection = mysql.createConnection({
-            host: 'localhost',
-            user: 'root',
-            password: '',
-            database: 'user'
-        });
-        connection.connect();
-        connection.query('SELECT * FROM user WHERE username="' + username + '"', function (err, rows) {
-            if (err) throw err;
-            var password;
-            if (rows.length != 0) {
-                password = rows[0].password;
+function promptLoginOrCreate() {
+    prompt.colors = false;
+    prompt.start();
+
+    prompt.get({
+        properties: {
+            answer: {
+                description: "Would you like to create a new login? (Y/n)"
             }
-            queryFinished(username,password);
-        });
-        connection.end();
-    }
+        }
+    }, function (err, result) {
+        if (err) throw err;
+
+        if (result.answer == 'Y' || result.answer == 'y') {
+            showLogin(function createLogin(result) {
+                db.usernameExistsInDb(result.username, function queryFinished(usernameExists) {
+                    if (!usernameExists) {
+                        db.addLoginToDb(result, function queryFinished(username) {
+                            // create user server directory
+                            fs.mkdirSync(argv.serverDirectory+"/"+username);
+                            directories.serverDirectory = argv.serverDirectory+"/"+username;
+                            directories.clientDirectory = argv.clientDirectory;
+                            scheduleChangeCheck(1000,true);
+                            getUserInput();
+                        });
+                    } else {
+                        console.log('Username already exists!\n');
+                        promptLoginOrCreate();
+                    }
+                });
+            });
+        } else if (result.answer == 'N' || result.answer == 'n') {
+            showLogin(function login(result) {
+                db.getPasswordFromDb(result.username, function queryFinished(login) {
+                    if (result.password != login.password) {
+                        console.log('Wrong login!\n');
+                        promptLoginOrCreate();
+                    } else {
+                        console.log('Login successful!\n');
+                        directories.serverDirectory = argv.serverDirectory+"/"+login.username;
+                        directories.clientDirectory = argv.clientDirectory;
+                        scheduleChangeCheck(1000,true);
+                        getUserInput();
+                    }
+                });
+            });
+        } else {
+            promptLoginOrCreate();
+        }
+    });
 }
 
-function promptLogin() {
+function showLogin(callback) {
     var schema = {
         properties: {
             username: {
@@ -187,25 +221,11 @@ function promptLogin() {
     prompt.get(schema, function (err, result) {
         if (err) throw err;
 
-        getPasswordFromDb(result.username)(
-            function queryFinished(username,password) {
-                if (result.password != password) {
-                    console.log("Wrong login!\n");
-                    promptLogin();
-                } else {
-                    console.log("Login successful!\n");
-                    directories.serverDirectory = argv.serverDirectory+"/"+username;
-                    directories.clientDirectory = argv.clientDirectory;
-                    console.log(directories.serverDirectory);
-                    console.log(directories.clientDirectory);
-                    scheduleChangeCheck(1000,true,username);
-                    getUserInput();
-                }
-            });
+        callback(result);
     });
 }
 
 dnodeClient.connect({host:argv.server, port:argv.port}, function(handler){
     sync.fsHandlers.dnode = handler;
-    promptLogin();
+    promptLoginOrCreate();
 });
